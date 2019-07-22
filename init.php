5f21d62cc38085c1cf049425a69b3375f59b4020
Pranathi-aytha <53084417+Pranathi-aytha@users.noreply.github.com>
2019-07-22 15:38:40 +0530

    Update init.php

--- init.php
+++ init.php
@@ -1,30 +1,30 @@
 <?php
-
-
 $db_name="id8600325_reservation";
 $pass="shouri25";
 $mysql_user="id8600325_pranathi";
 $server_name="localhost";
 
-$conn= new mysqli($server_name,$mysql_user,$pass,$db_name);
+$conn=mysqli_connect($server_name,$mysql_user,$pass,$db_name);
 // echo $conn;
-if($conn->connect_error)
-echo 'fail';
-else
-echo "success";
+// if($conn)
+// echo 'fail';
+// else
+// echo "success";
 if($conn)
 // {
-$sql="CREATE TABLE IF NOT EXISTS usertable(email text,username text,password text);" ;
 
+$sql="CREATE TABLE IF NOT EXISTS usertable(id integer primary key auto_increment,email varchar(100) unique key,username text,password text);" ;
 // $runsql=mysqli_query($conn,$SQL);
 
 // echo $runsql;
 // }
 
-if ($conn->query($sql)) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
+if (mysqli_query($conn,$sql)) {
+    $insert="insert into usertable values(1,'pranathi@gmail.com','pranathi','pranathi');";
+    mysqli_query($conn,$insert);
+//     echo "Table MyGuests created successfully";
+} 
+// else {
+//     echo "Error creating table: " . $conn->error;
+// }
 ?>
-<!-- INSERT INTO `usertable` (`email`, `username`, `password`) VALUES ('pranathi@gmail.com', 'pranathi', 'pranathi'); -->
