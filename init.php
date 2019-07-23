b467c3a2598234bcfe955d7966fce8cf18ac0dad
Pranathi-aytha <53084417+Pranathi-aytha@users.noreply.github.com>
2019-07-23 16:47:20 +0530

    Update init.php

--- init.php
+++ init.php
@@ -6,9 +6,9 @@ $server_name="localhost";
 $conn=mysqli_connect($server_name,$mysql_user,$pass,$db_name);
 if($conn)
 {
-$sql="CREATE TABLE IF NOT EXISTS usertable(id integer primary key auto_increment,email varchar(100) unique key,username text,password text);"
-$train="CREATE TABLE IF NOT EXISTS train(id integer primary key auto_increment,trainname varchar(100) unique key,fromstn varchar(100),tostn varchar(100));"
-$reserve="CREATE TABLE IF NOT EXISTS reservation(id integer primary key auto_increment,trainname varchar(100) unique key,date date);"
+$sql="CREATE TABLE IF NOT EXISTS usertable(id integer primary key auto_increment,email varchar(100) unique key,username text,password text);";
+$train="CREATE TABLE IF NOT EXISTS train(id integer primary key auto_increment,trainname varchar(100) unique key,fromstn varchar(100),tostn varchar(100));";
+$reserve="CREATE TABLE IF NOT EXISTS reservation(id integer primary key auto_increment,trainname varchar(100) unique key,date date);";
 
  }
 if (mysqli_query($conn,$sql)) {
