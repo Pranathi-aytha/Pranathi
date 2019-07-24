07d51b489504d5a5a6fe0df4fce277d90fedeccd
Pranathi-aytha <53084417+Pranathi-aytha@users.noreply.github.com>
2019-07-24 16:01:54 +0530

    Update train.php

--- train.php
+++ train.php
@@ -3,30 +3,11 @@ include "init1.php";
 $fromname="";
 $toname="";
 
- if(isset($_POST['from']) && isset($_POST['to'])){
-	$fromname= $_POST['from'];
-	$toname= $_POST['to'];
-}
-if(isset($_POST['find'])){
-	//echo $fromname;
-$sql = "SELECT * FROM traindisplay WHERE (fromstn='$fromname') and (tostn='$toname')";
-$result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
-echo "<br>";
-echo "<table border='1'>";
-if(mysqli_num_rows($result)>0){
-while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
-    echo "<tr>";
-        echo "Train No." . $row["trainno"] . "Train Name" . $row["trainname"] . "From Station" . $row["fromstn"] . "To Station" . $row["tostn"] . "Arr:" . $row["arrival"] . "Dep:" . $row["departure"] ."<br>"; // I just did not use "htmlspecialchars()" function. 
-    }
-    echo "</tr>";
-}
-echo "</table>";
-}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	
+
 </head>
 <body>
 	<div >
@@ -34,7 +15,7 @@ echo "</table>";
 		<div class="header">
 			<div style="float:left;width:20px;">
 				<img src="images/logo.jpg" height="270" width="300"/>
-			</div>		
+			</div>
 			<div>
 			<div style="float:right; font-size:20px;margin-top:20px;">
 </div>
@@ -43,12 +24,13 @@ echo "</table>";
 			</div>
 
 			</div>
-		</div>		
-			<center><method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+		</div>
+			<center>
+			<form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
 			<table class="table" style="margin-bottom:0px;">
 				<tr>
 					<th style="border-top:0px;"><label><b>Search Train</label></th>
-					<td id="mbox" style="border-top:0px;"> 
+					<td id="mbox" style="border-top:0px;">
 					<label>From </label></td>
 					<td style="border-top:0px;"><input  type="text" class="input-block-level" name="from" id="byn"></td>
 					<td id="xbox" style="border-top:0px;"><label> To </label></td>
@@ -57,7 +39,43 @@ echo "</table>";
 					<td style="border-top:0px;"><a class="btn btn-info" type="reset" value="Reset">Reset</a></td>
 				</tr>
 			</table>
+			<?php
 
+				 if(isset($_POST['from']) && isset($_POST['to'])){
+					$fromname= $_POST['from'];
+					$toname= $_POST['to'];
+				}
+				if(isset($_POST['find'])){
+					//echo $fromname;
+				$sql = "SELECT * FROM traindisplay WHERE (fromstn='$fromname') and (tostn='$toname')";
+				$result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
+				echo "<br>";
+				echo "<table border='1'>";
+				if(mysqli_num_rows($result)>0){
+					echo "<tr>";
+						echo "<th>Train No</th>";
+						echo "<th>Train Name</th>";
+						echo "<th>From Station</th>";
+						echo "<th>To Station</th>";
+						echo "<th>Arrival</th>";
+						echo "<th>Departure</th>";
+					echo "</tr>";
+				while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
+
+					echo "<tr>";
+						echo "<td>".$row['trainno']."</td>";
+						echo "<td>". $row['trainname']."</td>";
+						echo "<td>". $row['fromstn']."</td>";
+						echo "<td>".$row['tostn']."</td>";
+						echo "<td>".$row['arrival']."</td>";
+						echo "<td>".$row['departure']."</td>";
+					echo "</tr>";
+				    }
+
+				}
+				echo "</table>";
+				}
+			 ?>
 			</form>
 </center>
 
