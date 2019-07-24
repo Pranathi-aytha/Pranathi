bb655a3d174af1f5bc4de53d25c8dc2ed1cc83ff
Pranathi-aytha <53084417+Pranathi-aytha@users.noreply.github.com>
2019-07-24 10:15:32 +0530

    init.php

--- init.php
+++ init.php
@@ -6,13 +6,12 @@ $server_name="localhost";
 $conn=mysqli_connect($server_name,$mysql_user,$pass,$db_name);
 if($conn)
 {
-$sql="CREATE TABLE IF NOT EXISTS usertable(id integer primary key auto_increment,email varchar(100) unique key,username text,password text);";
-$train="CREATE TABLE IF NOT EXISTS train(id integer primary key auto_increment,trainname varchar(100) unique key,fromstn varchar(100),tostn varchar(100));";
-$reserve="CREATE TABLE IF NOT EXISTS reservation(id integer primary key auto_increment,trainname varchar(100) unique key,date date);";
-
+$sql="CREATE TABLE IF NOT EXISTS 'usertable'(id integer primary key auto_increment,email varchar(100) unique key,username text,password text);";
+$reserve="CREATE TABLE IF NOT EXISTS 'reservation'(reserveid integer primary key auto_increment,name varchar(100),age integer,gender varchar(100),remail varchar(100),dot date,dob date,amount integer);";
+$train="CREATE TABLE IF NOT EXISTS 'traindisplay'(trainid integer primary key auto_increment,train no integer unique key,trainname varchar(100) unique key,fromstn varchar(100),tostn varchar(100),arrival varchar(100),departure varchar(100),amount integer);";
  }
 if (mysqli_query($conn,$sql)) {
-    $insert="insert into usertable values(1,'pranathi@gmail.com','pranathi','pranathi');";
+    $insert="insert into table values(1,'pranathi@gmail.com','pranathi','pranathi');";
     mysqli_query($conn,$insert);
 } 
 if(mysqli_query($conn,$train)){
